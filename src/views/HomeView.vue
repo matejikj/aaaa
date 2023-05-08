e206f2fa8e9c3779455084a7ec677d412ae74a1f
Jakub Matejik <matejik@ttc.cz>
2023-05-08 23:03:26 +0200

    new

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container>
+    <h1>AAAAAAAAAA</h1>
+    <v-btn>dsaads</v-btn>
+  </v-container>
 </template>
